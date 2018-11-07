42712d27df2abacc4b3bbe95439399b712887575
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2018-11-07 15:20:06 +0100

    minor fixes

--- citableclass/base.py
+++ citableclass/base.py
@@ -13,10 +13,11 @@ import json
 import os
 import datetime
 
+
 def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
-    return sorted(l, key = alphanum_key)
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
 
 
 class Citableloader(object):
